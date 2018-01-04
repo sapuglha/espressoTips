1b6a9443b259f366d0e34a31e985d7f4b8764cfa
Tiago Silva <sapuglha@gmail.com>
2018-01-03 20:22:32 -0700

    Convert to Kotlin

---
+++ app/src/main/java/com/sapuglha/espressotips/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.sapuglha.espressotips
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import butterknife.OnClick
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            main_activity_text_output!!.text = savedInstanceState.getString(CURRENT_TEXT)
+        }
+    }
+
+    public override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        // Save the user's current game state
+        savedInstanceState.putString(CURRENT_TEXT, main_activity_text_output.text.toString())
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    @OnClick(R.id.main_activity_button_upper)
+    fun upper() {
+        val input = main_activity_text_input.text.toString()
+        main_activity_text_output.text = input.toUpperCase()
+
+        Snackbar.make(main_activity_parent_view, "You entered: " + input, Snackbar.LENGTH_LONG).show()
+    }
+
+    @OnClick(R.id.main_activity_button_lower)
+    fun lower() {
+        val input = main_activity_text_input.text.toString()
+        main_activity_text_output!!.text = input.toLowerCase()
+
+        if (main_activity_checkbox_new_window.isChecked) {
+            val intent = Intent(this@MainActivity, HelloActivity::class.java)
+            intent.putExtra("name", input)
+
+            startActivity(intent)
+            //            overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+        } else {
+            Snackbar.make(main_activity_parent_view, "You entered: " + input, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    companion object {
+        internal const val CURRENT_TEXT = "current_text"
+    }
+}
